bec6463e547e6d42f3190998fec5a57c09f16c1b
bsmg1h <438283168@qq.com>
2017-01-15 15:45:39 +0800

    test

---
+++ test.R
@@ -0,0 +1,6 @@
+# test
+
+# git remote add origin https://github.com/bsmg1h/test.git
+# git config remote.origin.url git@github.com:bsmg1h/test.git
+# git pull -u origin master
+# git push -u origin master
